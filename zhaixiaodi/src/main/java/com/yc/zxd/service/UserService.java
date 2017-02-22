6ecc04adfaee34c6b9f9d5cb5d272c22b64eca10
xiaokunji <1767822853@qq.com>
2017-02-22 21:41:51 +0800

    解决上传冲突问题--
    Signed-off-by: xiaokunji <1767822853@qq.com>

--- zhaixiaodi/src/main/java/com/yc/zxd/service/UserService.java
+++ zhaixiaodi/src/main/java/com/yc/zxd/service/UserService.java
@@ -1,19 +1,15 @@
 package com.yc.zxd.service;
 
-<<<<<<< HEAD
+import com.yc.zxd.entity.User;
 public interface UserService {
 
 	boolean isExistUN(String username);
 
 	String getMail(String tel);
+	
+	User login(User user);
 
 	boolean editPwdByTel(String tel, String password);
-=======
-import com.yc.zxd.entity.User;
 
-public interface UserService {
-
-	User login(User user);
->>>>>>> branch 'master' of https://github.com/soIlove/zhaixiaodi.git
 
 }
